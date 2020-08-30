9b329779d0587bd66d9e184bd883ab50b6d54a60
Russell <rdaswimmer@gmail.com>
2020-08-30 19:28:44 -0400

    first commit

---
+++ Lab2.java
@@ -0,0 +1,29 @@
+/* 
+Data Structures 2168 005
+Author Russell Abernethy
+Date: _____
+Program: Lab2 - List of Functions
+Desc: See PDF
+*/
+
+
+
+public class Lab2 {
+    public static void main(String[] args) {
+        
+    }
+// Uniqueness
+
+// All Multiples
+
+// All Strings of Size
+
+// isPermutation
+
+// String to List of Words
+
+// Remove All Instances
+
+
+    
+}
