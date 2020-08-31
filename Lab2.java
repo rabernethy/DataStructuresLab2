25bfa46f4edf9fb758ee606aae62ece568a47e7f
Russell <rdaswimmer@gmail.com>
2020-08-30 21:15:07 -0400

    Implemented the following methods: unique(), allMultiples(), allStringsOfSize(), isPermutation(), stringToListOfWords().

--- Lab2.java
+++ Lab2.java
@@ -6,6 +6,7 @@ Program: Lab2 - List of Functions
 Desc: See methods
 */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lab2 {
@@ -19,15 +20,20 @@ public class Lab2 {
             All the items are unique if none of them are the same.
         Return false otherwise.
         */
-
-
-
-
-        return false;
+        for(int i = 0; i < list.size() - 1; i++) {
+            // for each element in the list, compare it to every element after it.
+            E currentE = list.get(i);
+            for(int j = i+1; j < list.size(); j++) {
+                // if the current element matches, return false.
+                if(currentE == list.get(j)) 
+                    return false;
+            }
+        }
+        return true;
     }
 
 
-    public static ArrayList<Integer> allMultiples() {
+    public static List<Integer> allMultiples(List<Integer> list, int multiple) {
         /*
         Takes in a List of integers and an int.
         Returns a new List of integers which contains all of the numbers from the input 
@@ -35,8 +41,12 @@ public class Lab2 {
             For example,if the List is [1,25,2,5,30,19,57,2,25] and 5 was provided, 
             the new list should contain [25,5,30,25].
         */
-
-
+        List<Integer> output = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i) % multiple == 0)
+                output.add(list.get(i));
+        }
+        return output;
     }
 
 
@@ -49,10 +59,12 @@ public class Lab2 {
             ["I", "like", "to", "eat", "eat", "eat","apples", "and", "bananas"] and 3,
             The new list is: ["eat", "eat", "eat","and"].
         */
-    
-
-
-
+        List<String> output = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).length() == length)
+                output.add(list.get(i));
+        }
+        return output;
     }
 
 
@@ -64,14 +76,33 @@ public class Lab2 {
             duplicates, but they don’t have to contain the elements in the same order.
                 For example, [1,2, 4] and [2,1,4] are permutations of each other.
         */
-
-        return false;
+        // check that there are the same number of elements in each list
+        if(listA.size() != listB.size())
+            return false;
+        for(int i = 0; i <listA.size(); i++) {
+            // for every element in listA, check that the same number of that elment exists in both lists.
+            int countA = 0, countB = 0;
+            for(int j = 0; j < listA.size(); j++) {
+                // if the element at i in listA is the same as the element at j in either list, increment
+                // the corosponding list appropreatly.
+                if(listA.get(i) == listA.get(j))
+                    countA++;
+                if(listA.get(i) == listB.get(j))
+                    countB++;
+            }
+            // if there aren't the same number of occurances of the number of elements in each list,
+            // return false.
+            if (countA != countB) 
+                return false;
+        }
+        return true;
     }
 
 
     public static List<String> stringToListOfWords(String string) {
         /*
-        Takes in a String and converts it into a list of words.
+        Takes in a String.
+        Converts it into a list of words.
             We assumes that each word in the input string is separated by whitespace.
                 If our input String is "Hello, world!", then the output should be ["Hello,","world!"].
         This method returns List<String>.
@@ -80,8 +111,22 @@ public class Lab2 {
             Sanitize the String, cleaning it up so that we re-move the punctuation and other extraneous 
             characters such that the output in the above example would become["Hello", "world"]
         */
-
-
+        List<String> output = new ArrayList<>();
+        String curString = string;
+        while(true) {
+            // add the substring from the begining to the space.
+            output.add(curString.substring(0, curString.indexOf(' ')));
+            // change curString to curString - what was already added to the output.
+            curString = curString.substring(curString.indexOf(' '));
+            // if there are no more spaces, add everything left to the output list.
+            if(!curString.contains(" ")) {
+                output.add(curString);
+                break;
+            }
+        }
+        // sanatize output
+            //todo
+        return output;
     }
 
 
